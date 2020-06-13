dfadac149c0aba769a7a148a9bc9bdfe5641971f
André Yuri <andre_yuri@live.com>
2020-06-13 12:04:05 -0300

    Criando rotas de agendamento

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/NewAppointment/SelectProvider';
+import SelectDateTime from './pages/NewAppointment/SelectDateTime';
+import Confirm from './pages/NewAppointment/Confirm';
 
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
@@ -18,6 +26,35 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            NewAppointment: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255,0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
