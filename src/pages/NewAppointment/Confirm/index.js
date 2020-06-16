725f9113e267c9be434d9914b576fd1a6100fc36
André Yuri <andre_yuri@live.com>
2020-06-15 23:54:14 -0300

    Confirmando agendamento

--- src/pages/NewAppointment/Confirm/index.js
+++ src/pages/NewAppointment/Confirm/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
+import api from '~/services/api';
 
-const Confirm = () => {
-  return <View />;
+import { Container, Avatar, Name, Time, SubmitButtom } from './styles';
+
+const Confirm = ({ navigation }) => {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButtom onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButtom>
+      </Container>
+    </Background>
+  );
 };
 
 export default Confirm;
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirme agendamento',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
